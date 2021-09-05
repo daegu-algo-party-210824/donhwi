d4e0de755a75eb57d13dfee9c3810de0410e8505
donhwi94 <ehsgnl123@naver.com>
2021-09-05 18:01:46 +0900

    DFS/BFS - 특정 거리의 도시 찾기 문제 풀이

---
+++ DFS_BFS/특정거리의도시찾기.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+n, m, k, x = map(int, sys.stdin.readline().rstrip().split(" "))
+INF = 999999999
+distances = [INF] * (n + 1)
+graph = [[] for _ in range(n+1)]
+
+# 인접리스트 방식으로 연결되어 있는 노드를 표시
+for _ in range(m):
+    start, end = map(int, sys.stdin.readline().rstrip().split(" "))
+    graph[start].append(end)
+
+def dfs(v, distances):
+    queue = deque()
+    queue.append(v)
+    distances[v] = 0
+
+    while queue:
+        v = queue.popleft()
+        for i in range(len(graph[v])):
+            nv = graph[v][i]
+            if distances[nv] is INF: # 아직 방문하지 않았으면
+                queue.append(nv)
+
+            if distances[nv] > distances[v] + 1:
+                distances[nv] = distances[v] + 1
+
+dfs(x, distances)
+
+count = 0
+
+for index in range(1, len(distances)):
+    if distances[index] == k:
+        print(index)
+        count += 1
+
+if count == 0:
+    print(-1)
