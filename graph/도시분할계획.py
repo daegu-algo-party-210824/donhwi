32ac656aeeb81d5ae349ea56003e70406e60a021
donhwi94 <ehsgnl123@naver.com>
2021-09-24 03:02:33 +0900

    graph - 도시 분할 계획 문제 풀이

---
+++ graph/도시분할계획.py
@@ -0,0 +1,58 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+parent = [0] * (n+1)
+for i in range(1, n+1):
+    parent[i] = i
+
+edges = []
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+result = 0
+max = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+
+        if cost > max:
+            max = cost 
+
+        result += cost
+
+print(result - max)
+
+"""
+7 12
+1 2 3
+1 3 2
+3 2 1
+2 5 2
+3 4 4
+7 3 6
+5 1 5
+1 6 2 
+6 4 1
+6 5 3
+4 5 3
+6 7 4
+"""
