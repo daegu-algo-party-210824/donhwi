2deee040b76f0af6083441e0a95f93f24c0510c5
donhwi94 <ehsgnl123@naver.com>
2021-09-26 19:05:29 +0900

    graph - 어두운 길 문제 풀이

---
+++ graph/어두운길.py
@@ -0,0 +1,54 @@
+# 크루스칼 활용
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [0] * n
+for i in range(n):
+    parent[i] = i
+
+edges = []
+result = 0
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+    result += cost
+
+edges.sort()
+
+total_cost = 0
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        total_cost += cost
+
+print(result - total_cost)
+
+"""
+7 11
+0 1 7
+0 3 5
+1 2 8
+1 3 9
+1 4 7
+2 4 5
+3 4 15
+3 5 6
+4 5 8
+4 6 9
+5 6 11
+"""
+
+
