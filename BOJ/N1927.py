461c91860a63e23f3ebc84d88ac99a45aa1846aa
donhwi94 <ehsgnl123@naver.com>
2021-10-18 15:13:59 +0900

    heap - 최소 힙 문제 풀이

---
+++ BOJ/N1927.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+q = []
+for _ in range(n):
+    x = int(input())
+    if x != 0:
+        heapq.heappush(q, x)
+    elif x == 0:
+        if q:
+            value = heapq.heappop(q)
+            print(value)
+        else:
+            print(0)
