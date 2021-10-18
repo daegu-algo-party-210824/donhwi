5a6e6d0062cf7499d2542fd8e26d109e75afbff7
donhwi94 <ehsgnl123@naver.com>
2021-10-18 15:41:10 +0900

    heap - 최대 힙 문제 풀이

---
+++ BOJ/N11279.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+
+n = int(input())
+q = []
+
+for _ in range(n):
+    x = int(input())
+    if x != 0:
+        heapq.heappush(q, -x)
+    elif x == 0:
+        if q:
+            value = -heapq.heappop(q)
+            print(value)
+        else:
+            print(0)
+
